9c0bd4b20351dabaae3d7e1fee0a6aa915eb3f8e
Alexandru Grapa <alexandru22valentin@gmail.com>
2021-12-09 07:51:10 +0000

    Add pagination to views

--- quotesapi/quotesapi/settings.py
+++ quotesapi/quotesapi/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 30,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
